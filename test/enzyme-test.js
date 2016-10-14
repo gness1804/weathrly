59a1190dedccda428cf5dafdd86dc8b409c9a521
gness1804 <graham.nessler@gmail.com>
2016-10-13 20:00:54 -0600

    more testing setup

--- test/enzyme-test.js
+++ test/enzyme-test.js
@@ -1,6 +1,6 @@
 var expect = require('chai').expect;
 const React = require('react');
-const App   = require('../app/components/App');
+const App   = require('../lib/components/App');
 // const LikesCounter = require('../app/components/LikesCounter')
 // const ActionButton = require('../app/components/ActionButton');
 require('locus');
