ae9d133930a26aa2682f53c019c554b5dd757e4e
gness1804 <graham.nessler@gmail.com>
2016-10-13 19:42:34 -0600

    got virtual dom string into test setup file

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -7,22 +7,7 @@ require('babel-polyfill');
 //we need an instance of the document
 
 global.document = require('jsdom').jsdom(
-  "<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link rel="stylesheet" style="text/css" href="
-    https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.css"/>
-    <!--Google fonts link if wanted, available from https://fonts.google.com -->
-    <link rel="stylesheet" style="text/css" href="styles.css"/>
-    <title>Weathrly: Your World, Your Weather.</title>
-  </head>
-  <body>
-    <div id='application'></div>
-  <script src="main.bundle.js"></script>
-  </body>
-</html>
-
+  "<head><meta charset='utf-8'><meta name='viewport' content='width=device-width'/><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script><link rel='stylesheet' style='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.css'/>Google fonts link if wanted, available from https://fonts.google.com<link rel='stylesheet' style='text/css' href='styles.css'/><title>Weathrly: Your World, Your Weather.</title></head><body><div id='application'></div><script src='main.bundle.js'></script></body></html>"
 );
 
 //and an instance of the window
