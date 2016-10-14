d38efe4d8f08106a5178e29c3992863bdcef9737
gness1804 <graham.nessler@gmail.com>
2016-10-13 21:34:13 -0600

    trying to prettify text output

--- lib/components/GetWeather.jsx
+++ lib/components/GetWeather.jsx
@@ -45,7 +45,8 @@ class GetWeather extends React.Component {
     $.get("http://weatherly-api.herokuapp.com/api/weather", function (data) {
       let text = "";
       for (var i = min; i < max; i++) {
-        text = text + "In " + data[i].location + "," + " the weather on" + " " + data[i].date + " will be" + " " + data[i].weatherType.type;
+        text = <div>{data[i].location}</div>
+        // text = text + "In " + data[i].location + "," + " the weather on" + " " + data[i].date + " will be" + " " + data[i].weatherType.type;
       }
       that.setState({text: text});
 
