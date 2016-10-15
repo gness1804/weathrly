e27e62c7ce56a6e9cfc7469faac8929b85b5a5b2
Kristen Burgess <kristen.burgess@doane.edu>
2016-10-15 17:23:18 -0600

    WIP color and styling of weather day cards

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -92,8 +92,9 @@ class WeatherList extends React.Component {
   showWeatherData(data) {
 
     return(<div className="weather-card">
-      <p className="weather-card-text">
-        On {data.date}, the weather will be {data.weatherType.type} with a high of {data.temp.high} and a low of {data.temp.low}. There will be a {Math.floor(data.weatherType.chance * 100)} percent chance of this weather event happening.
+      <p className="weather-card-text1">
+        On {data.date}, the weather will be {data.weatherType.type} with a high of {data.temp.high} and a low of {data.temp.low}.</p>
+        <p className="weather-card-text2">There will be a {Math.floor(data.weatherType.chance * 100)} percent chance of this weather event happening.
       </p>
     </div>
     );
