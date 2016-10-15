caefe33fdc3c297e2e135cf0df8d33fcf840075f
gness1804 <graham.nessler@gmail.com>
2016-10-15 15:17:37 -0600

    adds math floor to percent chance

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -92,7 +92,7 @@ class WeatherList extends React.Component {
   showWeatherData(data) {
 
     return(<div>
-      On {data.date}, the weather will be {data.weatherType.type} with a high of {data.temp.high} and a low of {data.temp.low}. There will be a {data.weatherType.chance * 100} percent chance of this weather event happening.
+      On {data.date}, the weather will be {data.weatherType.type} with a high of {data.temp.high} and a low of {data.temp.low}. There will be a {Math.floor(data.weatherType.chance * 100)} percent chance of this weather event happening.
     </div>);
   }
 
