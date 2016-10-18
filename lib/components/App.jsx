5430f11adf73d003c0280c4cc698dad1f644fbe6
gness1804 <graham.nessler@gmail.com>
2016-10-17 20:38:12 -0600

    smarter way of passing in condition

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -101,17 +101,18 @@ class WeatherList extends React.Component {
   }
 
   showWeatherData(data) {
+    let condition = data.weatherType.type;
     return(<div className="weather-card">
             <p className='date'>{data.date}</p>
-            <img alt="sunny weather icon" className='symbol' src={chooseWeatherIcon(data)}/>
+            <img alt="sunny weather icon" className='symbol' src={chooseWeatherIcon(condition)}/>
             <p className="weather-card-text1"> The weather will be {data.weatherType.type} with a high of {data.temp.high} and a low of {data.temp.low}.</p>
             <p className="weather-card-text2">There will be a {Math.floor(data.weatherType.chance * 100)} percent chance of this weather event happening.
           </p>
         </div>)
 
 
-        function chooseWeatherIcon(data) {
-          console.log(data.weatherType.type);
+        function chooseWeatherIcon(condition) {
+          console.log(condition);
         }
 // if (data.weatherType.type === "sunny") {
 //     return(<div className="weather-card">
