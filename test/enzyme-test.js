246a7739cb3b4ca61b46d792bdd0f36f013202d5
gness1804 <graham.nessler@gmail.com>
2016-10-17 19:46:35 -0600

    small changes

--- test/enzyme-test.js
+++ test/enzyme-test.js
@@ -9,7 +9,7 @@ import {shallow, mount, render} from 'enzyme';
 describe('App.jsx rendering', function() {
   it('should render the weather for the given location', function() {
     const wrapper = mount(<App />)
-    var input = wrapper.find('#current-location-input').simulate('change', {target: {value: 'Denver'}})
+    var input = wrapper.find('#current-location-input').simulate('change', {target: {value: 'Denver'}});
     expect(wrapper.state('location')).to.equal('Denver');
   });
-});
+}); //end of App.jsx rendering
